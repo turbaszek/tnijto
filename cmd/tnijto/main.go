2bff3c3d5e97e93297981c3a8030fe69401bd9c4
Tomek Urbaszek <tomasz.urbaszek@polidea.com>
2020-08-17 22:00:48 +0200

    Simple UI

--- cmd/tnijto/main.go
+++ cmd/tnijto/main.go
@@ -1,11 +1,66 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	utils "github.com/turbaszek/tnijto/pkg"
 	"log"
 	"net/http"
+	"time"
 )
 
+var config = utils.NewConfig()
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-	log.Fatal(http.ListenAndServe(":1317", nil))
+	router := mux.NewRouter()
+
+	router.Handle("/", http.FileServer(http.Dir("./static")))
+	router.HandleFunc("/new", submitNewLinkHandler)
+
+	router.Use(utils.LoggingMiddleware)
+	router.NotFoundHandler = utils.Handle404()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":1317",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+// Link represents the link
+type Link struct {
+	URL          string
+	Name         string
+	GeneratedURL string
+}
+
+func submitNewLinkHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	link := r.FormValue("link")
+	name := r.FormValue("name")
+	generated := fmt.Sprintf("https://%s/%s", config.Hostname, name)
+
+	linkResponse := Link{link, name, generated}
+
+	js, err := json.Marshal(linkResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(js)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
---
+++ pkg/middleware.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"log"
+	"net/http"
+)
+
+// LoggingMiddleware is a logging middleware
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Printf("%s %s", r.Method, r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Handle404 handles 404 http error
+func Handle404() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+}
