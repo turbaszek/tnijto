fe4d4fc11455d7bcb9f3de696fc4178de6398262
Janusz Chludzinski <janusz.chludzinski@polidea.com>
2020-09-07 23:35:15 +0200

    swapping theme with switch button

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,82 +1,90 @@
 const emptyInput = "Musisz podać jakiś URL."
 
-function post(url, data, success) {
-    let params = typeof data == 'string' ? data : Object.keys(data).map(
-    function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
-    ).join('&');
+    function post(url, data, success) {
+        let params = typeof data == 'string' ? data : Object.keys(data).map(
+        function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+        ).join('&');
 
-    let xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-    xhr.open('POST', url);
-    xhr.onreadystatechange = function() {
-    if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
-};
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send(params);
-    return xhr;
-}
+        let xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+        xhr.open('POST', url);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
+        };
+
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send(params);
+        return xhr;
+    }
 
     function submitForm() {
-    let linkElement = document.getElementById("link")
-    let nameElement = document.getElementById("name")
-    let linkURL = linkElement.value
-    let value = nameElement.value
+        let linkElement = document.getElementById("link")
+        let nameElement = document.getElementById("name")
+        let linkURL = linkElement.value
+        let value = nameElement.value
 
-    if (linkURL !== "") {
-    post("/api/new", {'originalURL': linkURL, 'value': value}, showLink)
-    linkElement.value = ""
-    nameElement.value = ""
+        if (linkURL !== "") {
+            post("/api/new", {'originalURL': linkURL, 'value': value}, showLink)
+            linkElement.value = ""
+            nameElement.value = ""
 
-} else {
-    showMessage()
-}
-}
+        } else {
+            showMessage()
+        }
+    }
 
     function showLink(dataText) {
-    let data = JSON.parse(dataText)
-    console.log("Data txt: " + data)
-    let url = data.GeneratedURL
+        let data = JSON.parse(dataText)
+        let url = data.GeneratedURL
 
-    let msg = document.getElementById("generated-url");
-    let generatedURL = document.getElementById("generated-url-value");
+        let msg = document.getElementById("generated-url");
+        let generatedURL = document.getElementById("generated-url-value");
 
-    if (msg.style.display === "none") {
-    msg.style.display = "block";
-    generatedURL.textContent = url;
-} else {
-    generatedURL.textContent = url;
-}
+        if (msg.style.display === "none") {
+            msg.style.display = "block";
+            generatedURL.textContent = url;
 
-}
+        } else {
+            generatedURL.textContent = url;
+        }
+    }
 
     function showMessage() {
-    let messages = document.getElementById("messages-wrap");
+        let messages = document.getElementById("messages-wrap");
 
-    if(messages.classList.contains("hidden")) {
-    messages.classList.remove("hidden")
-    messages.classList.add("flex")
-    document.getElementById("error-message").textContent = emptyInput
-}
-}
+        if(messages.classList.contains("hidden")) {
+            messages.classList.remove("hidden")
+            messages.classList.add("flex")
+            document.getElementById("error-message").textContent = emptyInput
+        }
+    }
 
     window.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-    event.preventDefault();
-    submitForm();
-}
-});
+        if (event.key === "Enter") {
+            event.preventDefault();
+            submitForm();
+        }
+    });
 
     document.getElementById("submit-button").addEventListener("click", function(event){
-    event.preventDefault()
-    submitForm()
-});
+        event.preventDefault()
+        submitForm()
+    });
 
     document.getElementById("show-tailored-cut").addEventListener("click", function(event){
-    let icon = document.getElementById("tailored-cut-icon")
+        let icon = document.getElementById("tailored-cut-icon")
 
-    if (icon.classList.contains("fa-plus-circle")) {
-    icon.classList.replace("fa-plus-circle", "fa-minus-circle")
+        if (icon.classList.contains("fa-plus-circle")) {
+            icon.classList.replace("fa-plus-circle", "fa-minus-circle")
 
-} else {
-    icon.classList.replace("fa-minus-circle", "fa-plus-circle")
-}
-});
+        } else {
+            icon.classList.replace("fa-minus-circle", "fa-plus-circle")
+        }
+    });
+
+    document.getElementById("theme-switch").addEventListener("click", function (event){
+        let style = document.getElementById("style-select");
+
+        this.checked === true
+            ? style.setAttribute("href", "css/red-style.css")
+            : style.setAttribute("href", "css/dark-style.css")
+    })
